da9ce0cd45832bdbcdcecefb168f1f3e844a2e81
Alexandre Lefebvre <34lefebvre.alexandre@gmail.com>
2022-11-23 15:26:51 +0100

    commit de test

---
+++ script_cobaye.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class script_cobaye : MonoBehaviour
+{
+    // TU-nnel
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
